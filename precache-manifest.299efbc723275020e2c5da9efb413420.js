eb2f7a7cf76345a9ad3d042379e513a16d350710
utkutekalmaz <utkutekalmaz@gmail.com>
2020-02-28 17:49:40 +0300

    Updates

---
+++ precache-manifest.299efbc723275020e2c5da9efb413420.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1d3353acb429f11a3024d4c7a8ba9bb",
+    "url": "/personal/index.html"
+  },
+  {
+    "revision": "fdfb6c00875c2cd87116",
+    "url": "/personal/static/css/main.c672ce49.chunk.css"
+  },
+  {
+    "revision": "36629fdfbc003bd55851",
+    "url": "/personal/static/js/2.4291241f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/personal/static/js/2.4291241f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdfb6c00875c2cd87116",
+    "url": "/personal/static/js/main.084da335.chunk.js"
+  },
+  {
+    "revision": "d864ab305dc512e43673",
+    "url": "/personal/static/js/runtime-main.7bb36bd5.js"
+  },
+  {
+    "revision": "66d17b25d9c9d9f78f2546e3b2f53e58",
+    "url": "/personal/static/media/avatarsmall.66d17b25.jpg"
+  },
+  {
+    "revision": "50feef6bbf93065b775fba384962677a",
+    "url": "/personal/static/media/github.50feef6b.svg"
+  },
+  {
+    "revision": "64361c93ddcf8f388e975b4798f5ce31",
+    "url": "/personal/static/media/gmail.64361c93.svg"
+  },
+  {
+    "revision": "7b73924a8436394d55c85f0ceef33494",
+    "url": "/personal/static/media/linkedin.7b73924a.svg"
+  },
+  {
+    "revision": "41c7b258a6f941ae077d98ebf3b865dd",
+    "url": "/personal/static/media/skype.41c7b258.svg"
+  }
+]);
